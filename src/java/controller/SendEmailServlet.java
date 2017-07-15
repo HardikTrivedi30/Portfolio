43b771c89dd4a4ac5817db430741b28cc55fdbe6
Hardik Trivedi <hadditrivedi123@gmail.com>
2017-07-15 18:12:48 +0530

    Add files via upload

---
+++ src/java/controller/SendEmailServlet.java
@@ -0,0 +1,77 @@
+package controller;
+
+import DBO.MailDBO;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import service.SendMailService;
+
+@WebServlet(name = "SendEmailServlet", urlPatterns = {"/SendEmailServlet"})
+public class SendEmailServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        MailDBO mailDBO = new MailDBO();
+        System.out.println(request.getParameter("email") + request.getParameter("name") + request.getParameter("message") + request.getParameter("phone"));
+        mailDBO.setEmail(request.getParameter("email"));
+        mailDBO.setName(request.getParameter("name"));
+        mailDBO.setPhone(request.getParameter("phone"));
+        mailDBO.setMessageBody(request.getParameter("message"));
+        
+       SendMailService sendEmailService = new SendMailService();
+        sendEmailService.sendEmail(mailDBO);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
